f87e4c0388d8f73e10a337c20201370ea115b70f
cometin111 <cometin111@gmail.com>
2018-08-26 15:13:48 -0600

    Estructura inicial del proyecto

---
+++ config/global.php
@@ -0,0 +1,19 @@
+<?php 
+    //Ip de la pc servidor de base de datos
+    define("DB_HOST", "localhost");
+
+    //Nombre de la base de datos
+    define("DB_NAME", "viaticos");
+
+    //Usuario de la base de datos
+    define("DB_USERNAME", "root");
+
+    //Contraseña del usuario de la base de datos
+    define("DB_PASSWORD", "Sistemas25");
+
+    //definimos la codificacion de los caracteres
+    define("DB_ENCODE", "utf8mb4");
+
+    //Definimos una constante como nombre del proyecto
+    define("PRO_NOMBRE", "Control de Viaticos");
+?>
---
+++ vistas/starter.php
@@ -0,0 +1,18 @@
+<?php
+  require 'header.php';
+
+?>
+    <!-- Main content -->
+    <section class="content container-fluid">
+
+      <!--------------------------
+        | Your Page Content Here |
+        -------------------------->
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+<?php
+  require 'footer.php';
+?>
